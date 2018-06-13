7fda517dc8ec8da8b7b7356215a345158b1b0ca3
huangdinglun <2927786201@qq.com>
2018-06-13 09:57:40 +0800

    ok

---
+++ week1/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,19 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Document</title>
+ </head>
+ <body background="green">
+ 		<center>
+ 			<form action="<?php echo U('Index/index_do');?>" method="post">
+ 				<table>
+ 					<tr>
+ 						<td>留言</td>
+ 						<td><textarea name="" id="" cols="30" rows="10"></textarea></td>
+ 					</tr>
+ 				</table>
+ 			</form>
+ 		</center>
+ </body>
+ </html> s
